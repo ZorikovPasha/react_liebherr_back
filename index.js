0c7ef84f038baa98bf420b4744c4e7c1b032b8d6
ZorikovPasha <zorikov.pasha@mail.ru>
2022-06-13 15:31:36 +0300

    removed server from client folder

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dataRouter = require('./routes/dataRouter');
+const errorHandle = require("./middleware/error-handling")
+require('dotenv/config');
+
+const PORT = process.env.PORT ?? 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', dataRouter);
+app.use(errorHandle)
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },
+      () => console.log('connected to database')
+    )
+
+    app.listen(PORT, () => {
+      console.log('server has been started on port ' + PORT);
+    });
+
+  } catch(e) {
+    console.log(e);
+  }
+};
+start();
